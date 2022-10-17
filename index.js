88e9886a2ca05074e9acec197aee7eeeb4009e38
Zettacamp-Stefanus-Kevin <stefanus.kevin@zettacamp.pro>
2022-10-17 09:00:28 +0700

    add

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 console.log('hello world');
-var FirstName = "Learning Typescript is different than Javascript";
-console.log(FirstName);
+var sentence = "Learning Typescript is different than Javascript";
+console.log(sentence);
 function untukpotong(t, c, k) {
     var apa = t.slice(c, k);
     return apa;
 }
-console.log(untukpotong(FirstName, 0, 9));
-console.log(untukpotong(FirstName, 9, 19));
-console.log(untukpotong(FirstName, 19, 22));
-console.log(untukpotong(FirstName, 22, 32));
-console.log(untukpotong(FirstName, 32, 37));
-console.log(untukpotong(FirstName, 38, 50));
+console.log(untukpotong(sentence, 0, 9));
+console.log(untukpotong(sentence, 9, 19));
+console.log(untukpotong(sentence, 19, 22));
+console.log(untukpotong(sentence, 22, 32));
+console.log(untukpotong(sentence, 32, 37));
+console.log(untukpotong(sentence, 38, 50));
+var duanda = true;
+console.log(duanda);
+var farhan = 'tester';
+console.log(farhan);
--- index.ts
+++ index.ts
@@ -1,27 +1,38 @@
 console.log('hello world')
 let sentence :string= "Learning Typescript is different than Javascript";
 
+let a;
+a = 6;
 
 console.log(sentence)
 
-function untukpotong(t:string,c:number,k:number){
-    let apa : string = t.slice(c,k)
-    return apa;
+function potong(text,num1:number,num2:number):string{
+    let word : string = text.slice(num1,num2)
+    return word;
 }
 
-console.log(untukpotong(sentence,0,9));
-console.log(untukpotong(sentence,9,19));
-console.log(untukpotong(sentence,19,22));
-console.log(untukpotong(sentence,22,32));
-console.log(untukpotong(sentence,32,37));
-console.log(untukpotong(sentence,38,50));
+// console.log(potong(sentence,0,9));
+console.log(potong(sentence,9,19));
+// console.log(potong(sentence,19,22));
+// console.log(potong(sentence,22,32));
+// console.log(potong(sentence,32,37));
+// console.log(potong(sentence,38,50));
 
 
 
-let duanda : boolean = true
 
-console.log(duanda);
 
-let farhan : any = 'tester'
 
-console.log(farhan)
+// let assign : string ="ini assgin"
+// console.log(assign)
+// let inferen = "ini inferen"
+// console.log(inferen)
+
+// let duanda : boolean = true
+
+// console.log(duanda);
+
+// let farhan : any = 'tester'
+
+// console.log(farhan)
+
