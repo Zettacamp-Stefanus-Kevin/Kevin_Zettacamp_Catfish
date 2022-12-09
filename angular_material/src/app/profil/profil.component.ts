0b64cb74b115735fbc09bc8a4d75f77199706597
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-12-09 08:45:10 +0700

    [PROFILE] - Remove all console.log

--- angular_material/src/app/profil/profil.component.ts
+++ angular_material/src/app/profil/profil.component.ts
@@ -5,35 +5,29 @@ import { ProfilService } from './profil.service';
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.css']
+  styleUrls: ['./profil.component.css'],
 })
 export class ProfilComponent implements OnInit {
-
-  hide = true
+  hide = true;
   private subs = new SubSink();
   data: any;
   email: any;
 
-  constructor(private profilService: ProfilService) { }
+  constructor(private profilService: ProfilService) {}
 
   ngOnInit(): void {
-    this.init()
+    this.init();
   }
 
   init() {
-    this.email = localStorage.getItem('email')
-    console.log(this.email);
-    this.subs.sink = this.profilService.getUser(this.email).valueChanges.subscribe((resp: any) => {
-      console.log(resp);
-      
-      this.data = resp?.data?.GetOneUser
-      console.log(this.data);
-      
-    })
+    this.email = localStorage.getItem('email');
+
+    this.subs.sink = this.profilService
+      .getUser(this.email)
+      .valueChanges.subscribe((resp: any) => {
+        this.data = resp?.data?.GetOneUser;
+      });
   }
 
-  Edit() {
-
-  }
-
+  Edit() {}
 }
--- angular_material/src/app/profil/profil.service.ts
+++ angular_material/src/app/profil/profil.service.ts
@@ -2,33 +2,30 @@ import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfilService {
-
-  constructor(private apolo: Apollo) { }
+  constructor(private apolo: Apollo) {}
 
   getUser(data: any) {
-    console.log(data);
-    
     return this.apolo.watchQuery({
       query: gql`
-      query Query($email: String) {
-        GetOneUser(email: $email) {
-          id
-          first_name
-          last_name
-          email
-          password
-          balance
-          question_answer
+        query Query($email: String) {
+          GetOneUser(email: $email) {
+            id
+            first_name
+            last_name
+            email
+            password
+            balance
+            question_answer
+          }
         }
-      }
-      `, variables: {
-        email: data
+      `,
+      variables: {
+        email: data,
       },
-      fetchPolicy: 'network-only'
-    })
+      fetchPolicy: 'network-only',
+    });
   }
-
 }
