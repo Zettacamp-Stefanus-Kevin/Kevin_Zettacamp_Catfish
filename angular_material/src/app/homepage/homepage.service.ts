2d8aa67d0e84b4eed064e4aa42bc4e2f646f819c
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-12-09 08:48:55 +0700

    [HOMEPAGE] - Sprucing up the homepage style

--- angular_material/src/app/homepage/homepage.service.ts
+++ angular_material/src/app/homepage/homepage.service.ts
@@ -2,61 +2,59 @@ import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomepageService {
-
-  constructor(private apolo : Apollo) { }
-
+  constructor(private apolo: Apollo) {}
 
   getMenu() {
     return this.apolo.watchQuery({
       query: gql`
-      query Query($isHightlighted: Boolean) {
-        GetAllRecipesNotLogin(is_hightlighted: $isHightlighted) {
-          data_recipes {
-            id
-            description
-            image
-            price
-            recipe_name
-            status
-            is_hightlighted
-            is_special_offers{
+        query Query($isHightlighted: Boolean) {
+          GetAllRecipesNotLogin(is_hightlighted: $isHightlighted) {
+            data_recipes {
+              id
+              description
+              image
+              price
+              recipe_name
               status
-              discount
+              is_hightlighted
+              is_special_offers {
+                status
+                discount
+              }
             }
           }
         }
-      }
-      
-      `, variables : {"isHightlighted": false},
-    })
+      `,
+      variables: { isHightlighted: false },
+    });
   }
 
   getDiskon() {
     return this.apolo.watchQuery({
       query: gql`
-      query Query($isHightlighted: Boolean) {
-        GetAllRecipesNotLogin(is_hightlighted: $isHightlighted) {
-          data_recipes {
-            id
-            description
-            image
-            price
-            recipe_name
-            status
-            is_hightlighted
-            is_special_offers{
+        query Query($isHightlighted: Boolean) {
+          GetAllRecipesNotLogin(is_hightlighted: $isHightlighted) {
+            data_recipes {
+              id
+              description
+              image
+              price
+              recipe_name
               status
-              discount
+              is_hightlighted
+              is_special_offers {
+                status
+                discount
+                price_discount
+              }
             }
           }
         }
-      }
-      
-      `, variables : {"isSpecialOffers": false},
-    })
+      `,
+      variables: { isSpecialOffers: false },
+    });
   }
-
 }
