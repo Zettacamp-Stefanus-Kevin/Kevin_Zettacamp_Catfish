e837919c4929d58567539e9a64c1cc3a7088758f
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-12-09 08:46:37 +0700

    [ROUTING] - Comment about routing

--- angular_material/src/app/app-routing.module.ts
+++ angular_material/src/app/app-routing.module.ts
@@ -1,24 +1,78 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartGuard } from './cart/cart.guard';
 import { RoleGuard } from './login/role.guard';
 import { TokenGuard } from './login/token.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: 'homepage', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
-  { path: 'profil', loadChildren: () => import('./profil/profil.module').then(m => m.ProfilModule) },
-  { path: 'menu', loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule) },
-  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule), canActivate:[TokenGuard]},
-  { path: 'stock_management', loadChildren: () => import('./stock-management/stock-management.module').then(m => m.StockManagementModule), canActivate:[TokenGuard , RoleGuard] },
-  { path: 'menu_management', loadChildren: () => import('./menu-management/menu-management.module').then(m => m.MenuManagementModule), canActivate:[TokenGuard , RoleGuard] },
-  { path: 'transaction_management', loadChildren: () => import('./transaction-management/transaction-management.module').then(m => m.TransactionManagementModule), canActivate:[TokenGuard , RoleGuard] },
+  {
+    path: 'homepage',
+    loadChildren: () =>
+      import('./homepage/homepage.module').then((m) => m.HomepageModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./register/register.module').then((m) => m.RegisterModule),
+  },
+  {
+    path: 'profil',
+    loadChildren: () =>
+      import('./profil/profil.module').then((m) => m.ProfilModule),
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then((m) => m.MenuModule),
+  },
+  // {
+  //   path: 'about',
+  //   loadChildren: () =>
+  //     import('./about/about.module').then((m) => m.AboutModule),
+  // },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then((m) => m.CartModule),
+    canActivate: [CartGuard],
+  },
+  {
+    path: 'stock_management',
+    loadChildren: () =>
+      import('./stock-management/stock-management.module').then(
+        (m) => m.StockManagementModule
+      ),
+    canActivate: [TokenGuard, RoleGuard],
+  },
+  {
+    path: 'menu_management',
+    loadChildren: () =>
+      import('./menu-management/menu-management.module').then(
+        (m) => m.MenuManagementModule
+      ),
+    canActivate: [TokenGuard, RoleGuard],
+  },
+  {
+    path: 'transaction_management',
+    loadChildren: () =>
+      import('./transaction-management/transaction-management.module').then(
+        (m) => m.TransactionManagementModule
+      ),
+    canActivate: [TokenGuard, RoleGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/homepage',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
